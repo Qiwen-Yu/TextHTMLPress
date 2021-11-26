2ff8bbe1667b27a3380a7378e6a2b90aad36cd83
Qiwen-Yu <yvaineyuu.0201@gmail.com>
2021-11-26 16:15:37 -0500

    change test structure

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 # import pytest
 # from click.testing import CliRunner
-from TextHTMLPress import Generator, get_title
+from TextHTMLPress import Generator, get_title, get_paragraphs
 from pathlib import Path
 
 
@@ -11,7 +11,13 @@ from pathlib import Path
 #     assert result.exit_code == 0
 
 
-def test_title():
+def test_get_paragraphs() -> None:
+    filepath = Path("./inputs/test1.txt")
+    para = get_paragraphs(filepath)
+    assert para == ["Silver Blaze", "\nI am afraid, Watson, that I shall have to go"]
+
+
+def test_title() -> None:
     filepath = Path("./inputs/Silver_Blaze.txt")
     title = get_title(filepath)
     assert title == "Silver Blaze"
@@ -21,9 +27,7 @@ def test_title():
     assert title == "Zen"
 
 
-def test_md():
-    # works in pytest
-    # inp = Path("./tests/inputs/test1.txt")
+def test_md() -> None:
     inp = Path("./inputs/test1.txt")
     output = Path("../dist")
     stylesheet = ""
@@ -35,4 +39,16 @@ def test_md():
         == """<!DOCTYPE html><html lang="en-CA"><head><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Silver Blaze</title></head><body><h1>Silver Blaze</h1><p>
 I am afraid, Watson, that I shall have to go</p></body></html>"""
     )
+
+
+def test_generate_html_from_md() -> None:
+    inp = Path("./inputs/example1.md")
+    output = Path("./dist")
+    expected_output_file = Path("./dist/example1.html")
+    stylesheet = ""
+    lang = "fr"
+    g = Generator(inp, output, stylesheet, lang)
+    g.parse_markdown(inp)
+    g.generator_wrapper()
+    assert expected_output_file.exists()
 
